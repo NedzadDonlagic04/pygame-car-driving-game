1d9be83e8b487f3fd1d1003e8b2a098afdc4b12c
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-02-07 15:39:03 +0100

    Added global variable, added RandomEnemy class
    RandomEnemy class is used to generate a random
    enemy on the screen.

--- classes.py
+++ classes.py
@@ -1,8 +1,12 @@
 import pygame
+import random
 
 # Defining the scale of the car
 # Used by Player and Enemy classes
 CAR_SCALE = (100, 150)
+
+# Used to offset the top and bottom parts of the car from screen edge
+OFFSET_CARS = 5
 
 class MyClock:
     def __init__(self, fps) -> None:
@@ -13,8 +17,8 @@ class MyClock:
         self.CLOCK.tick(self.FPS)
 
 class BackgroundImage:
-    def __init__(self, width, height, path) -> None:
-        self.IMAGE = pygame.image.load(path).convert_alpha()
+    def __init__(self, width, height) -> None:
+        self.IMAGE = pygame.image.load('./img/road.png').convert_alpha()
         self.IMAGE = pygame.transform.scale(self.IMAGE, (width, height))
         self.HEIGHT = height
         self.movableY = 0
@@ -29,9 +33,9 @@ class BackgroundImage:
             self.movableY = 0
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, width, height, path) -> None:
+    def __init__(self, width, height) -> None:
         super().__init__()
-        self.image = pygame.image.load(path).convert_alpha()
+        self.image = pygame.image.load('./img/player.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, CAR_SCALE)
 
         # Used to calculate what part of the image of the road can be driven on
@@ -39,15 +43,15 @@ class Player(pygame.sprite.Sprite):
         percent = width * 0.2
 
         # Used to offset the top and bottom parts of the car from screen edge
-        offset = 5
+        OFFSET_CARS = 5
 
         self.RIGHT_BORDER = width - percent
         self.LEFT_BORDER = percent
-        self.TOP_BORDER = offset
-        self.BOTTOM_BORDER = height - offset
+        self.TOP_BORDER = OFFSET_CARS
+        self.BOTTOM_BORDER = height - OFFSET_CARS
 
-        self.SPEED = 4
-
+        self.SPEED = 5
+        
         self.rect = self.image.get_rect( bottomleft = (self.LEFT_BORDER, self.BOTTOM_BORDER) )
 
     def update(self) -> None:
@@ -73,3 +77,32 @@ class Player(pygame.sprite.Sprite):
     def draw(self, screen) -> None:
         screen.blit(self.image, self.rect)
 
+class RandomEnemy(pygame.sprite.Sprite):
+    def __init__(self, width) -> None:
+        super().__init__()
+
+        rng = random.choice([1, 2])
+
+        self.image = pygame.image.load(f'./img/enemy_{rng}.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, CAR_SCALE)
+
+        # Used to calculate what part of the image of the road can be driven on
+        # and which can't be driven on
+        # Index 0 is for the cars in left track
+        # The top point is 20% of the whole width
+        # Index 1 is for the cars in the right track
+        # The top point is the entire width - 40% of the entire width
+        percent = [width * 0.2, width - width * 0.4]
+
+        # Used to offset the top and bottom parts of the car from screen edge
+        OFFSET_CARS = 5
+
+        self.SPEED = 5
+
+        self.rect = self.image.get_rect( topleft = (percent[rng - 1], OFFSET_CARS) )
+
+    def update(self) -> None:
+        self.rect.bottom += self.SPEED
+
+    def draw(self, screen) -> None:
+        screen.blit(self.image, self.rect)
