8773e25c604c6e2ed59df18c958eb2dc624ac459
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-02-08 09:49:20 +0100

    Implemented game over with enemy player collision

--- main.py
+++ main.py
@@ -18,10 +18,13 @@ class Game:
 
         self.PLAYER = Player(width, height)
 
-        self.ENEMY = RandomEnemy(width)
+        self.enemyGroup = pygame.sprite.Group()
+        self.enemySpawnTime = 2000
+        self.enemyCurrentSpawnTime = 0
 
     def quit(self) -> None:
         pygame.quit()
+        exit()
     
     def run(self) -> None:
         while True:
@@ -34,8 +37,17 @@ class Game:
             self.PLAYER.update()
             self.PLAYER.draw(self.SCREEN)
 
-            self.ENEMY.update()
-            self.ENEMY.draw(self.SCREEN)
+            if pygame.time.get_ticks() - self.enemyCurrentSpawnTime >= self.enemySpawnTime:
+                self.enemyGroup.add( RandomEnemy(self.WIDTH, self.HEIGHT) )
+                self.enemyCurrentSpawnTime = pygame.time.get_ticks()
+
+            self.enemyGroup.update()
+            self.enemyGroup.draw(self.SCREEN)
+
+            collision = pygame.sprite.spritecollide(self.PLAYER, self.enemyGroup, False)
+
+            if collision:
+                self.quit()
 
             pygame.display.update()
             self.CLOCK.tick()
